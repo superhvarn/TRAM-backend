f296a1e8d37771e7bfca78db12067958eea19516
superhvarn <56454934+superhvarn@users.noreply.github.com>
2023-11-13 14:45:52 -0500

    Update utils.py
    Fixes #2 and using config.yaml

--- url_shortener/utils.py
+++ url_shortener/utils.py
@@ -3,12 +3,8 @@ import random
 import functools
 from flask import jsonify, request
 from .models import User, URL
+import yaml
 
-# Following the assignment's rate limits
-RATE_LIMITS = {
-    'Tier 1': 1000,
-    'Tier 2': 100
-}
 
 def generate_short_link(num_chars=6):
     return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(num_chars))
@@ -16,19 +12,27 @@ def generate_short_link(num_chars=6):
 
 def check_rate_limit(user_id):
     def decorator(f):
-        @functools.wraps(f)
+            
+       
+        @functools.wraps(f) 
         def decorated_function(*args, **kwargs):
+            with open('config.yaml') as m:
+                myconfig = yaml.safe_load(m)
+                
             user = User.query.get(user_id)
             if not user:
                 return jsonify({'error': 'User not found'}), 404
 
             tier = user.tier
+            
+            # fetching rate limits from config file
+            tierLimit = myconfig['RATE_LIMITS']["Tier " + str(tier)]
             request_count = URL.query.filter_by(user_id=user_id).count()
 
-            if request_count > RATE_LIMITS[f'Tier {tier}']:
-                return jsonify({'error': 'Rate limit exceeded'}), 429
+            if request_count > tierLimit:
+                return jsonify({'error': 'Rate limit exceeded'}, 429)
 
-            print(f"User {user_id} has made {request_count} requests out of {RATE_LIMITS[f'Tier {tier}']} allowed.")
+            print(f"User {user_id} has made {request_count} requests out of {str(tierLimit)} allowed.")
 
             return f(*args, **kwargs)
         return decorated_function
