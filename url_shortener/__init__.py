0d9811f5e61b2d356a428c88095505873a96ffa9
superhvarn <56454934+superhvarn@users.noreply.github.com>
2023-11-13 14:44:31 -0500

    Update __init__.py
    Using config.yaml

--- url_shortener/__init__.py
+++ url_shortener/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import yaml
 
 db = SQLAlchemy()  # Initializing the databaase
 
 def create_app():
     app = Flask(__name__, static_folder='static')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///url_shortener.db'
+    
+    # configuration info will be stored in a yaml file
+    with open('config.yaml') as f:
+        myconfig = yaml.safe_load(f)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['STATIC_FOLDER'] = 'url_shortener/static'
+    app.config['SQLALCHEMY_DATABASE_URI'] = myconfig['dburi']
 
     db.init_app(app)  # Binding the database with the app
 
